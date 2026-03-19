6d47f48321d6a26966c8a6cdba45e6292c479413
František Bereň <birko@live.com>
2026-03-19 19:09:07 +0100

    Add missing using directive for Birko.Configuration in TestTranslationStore

--- TestTranslationStore.cs
+++ TestTranslationStore.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Birko.Data.Stores;
+using Birko.Configuration;
 using Birko.Localization.Data;
 
 namespace Birko.Localization.Data.Tests;
